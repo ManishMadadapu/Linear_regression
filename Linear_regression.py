fa8ae077d42f005b1bd3a3bbd05d0ce2c58375ed
Manish M <35169362+ManishMadadapu@users.noreply.github.com>
2018-07-29 18:18:03 +0530

    Update Linear_regression.py

--- Linear_regression.py
+++ Linear_regression.py
@@ -1,14 +1,15 @@
 from numpy import *
 
-t_0 = 0
-t_1 = 0
+t_0 = 0 #Parameter 1
+t_1 = 0 #Parameter 2
 learning_rate = 0.0001
 iterations = 1000
-txt = genfromtxt('data.csv',delimiter = ',')
-data = array(txt)
-temp_0 = 0 
-temp_1 = 0
+data = array(genfromtxt('data.csv',delimiter = ',')) # data coverted into the form of an array
+temp_0 = 0 # temporary prameter
+temp_1 = 0# temporary prameter
 m = len(data)
+
+# gradient descent
 for i in range(iterations):
     for i in range(m):
                 x = data[i,0]
@@ -17,5 +18,8 @@ for i in range(iterations):
                 temp_1 -= learning_rate*((1/m*((t_0 + t_1*x)-y) )*x)
     t_0 = temp_0
     t_1 = temp_1
+    
+    
+   #prints the final result of the gradient descent
 print("t_0:", t_0)
 print("t_1:",t_1)
